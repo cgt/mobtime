3a3991c7d3160670cf4250a8b50f88a90a1b377f
Christoffer G. Thomsen <chris@cgt.name>
2020-12-05 11:51:34 +0100

    Extract function

--- src/update.js
+++ src/update.js
@@ -9,7 +9,7 @@ const defaultStatistics = {
   connections: 0,
 };
 
-const extractStatistics = (message) => {
+const extractStatistics = message => {
   const { type, ...data } = JSON.parse(message);
 
   switch (type) {
@@ -24,167 +24,171 @@ const extractStatistics = (message) => {
   }
 };
 
-
-export const update = (action, state) => Action.caseOf({
-  Init: () => [
+export const update = (action, state) =>
+  Action.caseOf(
     {
-      connections: [],
-      statistics: {},
-    },
-    effects.none(),
-  ],
-
-  AddConnection: (websocket, timerId) => {
-    const connection = Connection.make(
-      websocket,
-      timerId,
-    );
-
-    return [
-      {
-        ...state,
-        connections: state.connections.concat(connection),
-      },
-      effects.batch([
-        Action.SetTimerOwner(timerId),
-        Action.UpdateStatisticsFromConnections(timerId),
-      ]),
-    ];
-  },
-
-  RemoveConnection: (websocket, timerId) => {
-    const connections = state.connections.filter((c) => c.getWebsocket() !== websocket);
-
-    return [
-      {
-        ...state,
-        connections,
-      },
-      effects.batch([
-        effects.thunk(() => {
-          websocket.close();
-          return effects.none();
-        }),
-        Action.SetTimerOwner(timerId),
-        Action.UpdateStatisticsFromConnections(timerId),
-      ]),
-    ];
-  },
-
-  MessageTimer: (websocket, timerId, message) => {
-    const connections = state.connections.filter((connection) => (
-      connection.timerId === timerId
-        && connection.getWebsocket() !== websocket
-    ));
-
-    return [
-      state,
-      effects.batch([
-        effects.thunk(() => {
-          connections.forEach((connection) => {
-            connection.getWebsocket().send(message);
-          });
-          return effects.none();
-        }),
-        Action.UpdateStatisticsFromMessage(timerId, message),
-      ]),
-    ];
-  },
-
-  MessageTimerOwner: (websocket, timerId, message) => {
-    const owner = state.connections.find((c) => (
-      c.timerId === timerId && c.isOwner
-    ));
-
-    return [
-      state,
-      effects.thunk(() => {
-        if (owner && owner.getWebsocket() !== websocket) {
-          owner.getWebsocket().send(message);
-        }
-        return effects.none();
-      }),
-    ];
-  },
-
-  UpdateStatisticsFromMessage: (timerId, message) => {
-    const statistics = {
-      ...state.statistics,
-      [timerId]: {
-        ...defaultStatistics,
-        ...state.statistics[timerId],
-        ...extractStatistics(message),
-      },
-    };
-
-    return [
-      {
-        ...state,
-        statistics,
-      },
-      effects.none(),
-    ];
-  },
-
-  UpdateStatisticsFromConnections: (timerId) => {
-    const connections = state.connections.filter((connection) => (
-      connection.timerId === timerId
-    ));
-
-    const { [timerId]: old, ...prevStatistics } = state.statistics;
-
-    const augment = connections.length > 0
-      ? {
-        [timerId]: {
-          ...defaultStatistics,
-          ...old,
-          connections: connections.length,
+      Init: () => [
+        {
+          connections: [],
+          statistics: {},
         },
-      }
-      : {};
-
-    const statistics = {
-      ...prevStatistics,
-      ...augment,
-    };
-
-    return [
-      {
-        ...state,
-        statistics,
-      },
-      effects.none(),
-    ];
-  },
-
-  SetTimerOwner: (timerId) => {
-    const targetIndex = state.connections.findIndex((c) => c.timerId === timerId);
-    if (targetIndex < 0) {
-      return [
-        state,
         effects.none(),
-      ];
-    }
+      ],
 
-    const connections = state.connections.map((connection, index) => ({
-      ...connection,
-      isOwner: targetIndex === index ? true : connection.isOwner,
-    }));
+      AddConnection: (websocket, timerId) => {
+        const connection = Connection.make(websocket, timerId);
 
-    const timerConnections = connections.filter((c, index) => (
-      c.timerId === timerId
-        && index !== targetIndex
-    ));
-
-    return [
-      {
-        ...state,
-        connections,
+        return [
+          {
+            ...state,
+            connections: state.connections.concat(connection),
+          },
+          effects.batch([
+            Action.SetTimerOwner(timerId),
+            Action.UpdateStatisticsFromConnections(timerId),
+          ]),
+        ];
       },
-      effects.batch([
-        SendOwnership(connections[targetIndex], true),
-        ...timerConnections.map((tc) => SendOwnership(tc, false)),
-      ]),
-    ];
-  },
-}, action);
+
+      RemoveConnection: (websocket, timerId) => {
+        const connections = state.connections.filter(
+          c => c.getWebsocket() !== websocket,
+        );
+
+        return [
+          {
+            ...state,
+            connections,
+          },
+          effects.batch([
+            effects.thunk(() => {
+              websocket.close();
+              return effects.none();
+            }),
+            Action.SetTimerOwner(timerId),
+            Action.UpdateStatisticsFromConnections(timerId),
+          ]),
+        ];
+      },
+
+      MessageTimer: (websocket, timerId, message) => {
+        const connections = state.connections.filter(
+          connection =>
+            connection.timerId === timerId &&
+            connection.getWebsocket() !== websocket,
+        );
+
+        return [
+          state,
+          effects.batch([
+            effects.thunk(() => {
+              connections.forEach(connection => {
+                connection.getWebsocket().send(message);
+              });
+              return effects.none();
+            }),
+            Action.UpdateStatisticsFromMessage(timerId, message),
+          ]),
+        ];
+      },
+
+      MessageTimerOwner: (websocket, timerId, message) => {
+        const owner = state.connections.find(
+          c => c.timerId === timerId && c.isOwner,
+        );
+
+        return [
+          state,
+          effects.thunk(() => {
+            if (owner && owner.getWebsocket() !== websocket) {
+              owner.getWebsocket().send(message);
+            }
+            return effects.none();
+          }),
+        ];
+      },
+
+      UpdateStatisticsFromMessage: (timerId, message) => {
+        const statistics = {
+          ...state.statistics,
+          [timerId]: {
+            ...defaultStatistics,
+            ...state.statistics[timerId],
+            ...extractStatistics(message),
+          },
+        };
+
+        return [
+          {
+            ...state,
+            statistics,
+          },
+          effects.none(),
+        ];
+      },
+
+      UpdateStatisticsFromConnections: timerId => {
+        const connections = state.connections.filter(
+          connection => connection.timerId === timerId,
+        );
+
+        const { [timerId]: old, ...prevStatistics } = state.statistics;
+
+        const augment =
+          connections.length > 0
+            ? {
+                [timerId]: {
+                  ...defaultStatistics,
+                  ...old,
+                  connections: connections.length,
+                },
+              }
+            : {};
+
+        const statistics = {
+          ...prevStatistics,
+          ...augment,
+        };
+
+        return [
+          {
+            ...state,
+            statistics,
+          },
+          effects.none(),
+        ];
+      },
+
+      SetTimerOwner: timerId => {
+        function findFirstConnectionToTimer() {
+          return state.connections.findIndex(c => c.timerId === timerId);
+        }
+
+        const targetIndex = findFirstConnectionToTimer();
+        if (targetIndex < 0) {
+          return [state, effects.none()];
+        }
+
+        const connections = state.connections.map((connection, index) => ({
+          ...connection,
+          isOwner: targetIndex === index ? true : connection.isOwner,
+        }));
+
+        const timerConnections = connections.filter(
+          (c, index) => c.timerId === timerId && index !== targetIndex,
+        );
+
+        return [
+          {
+            ...state,
+            connections,
+          },
+          effects.batch([
+            SendOwnership(connections[targetIndex], true),
+            ...timerConnections.map(tc => SendOwnership(tc, false)),
+          ]),
+        ];
+      },
+    },
+    action,
+  );
